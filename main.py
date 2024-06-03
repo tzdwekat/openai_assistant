485b30c5d3c70748ef6b328023f71dfe12474199
Tariq Dwekat <tariqzdwekat@gmail.com>
2024-06-02 22:08:53 -0500

    yea

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 #imports
 import openai
+import streamlit as st
 import aiohttp  # for making API calls concurrently
 import argparse  # for running script from command line
 import asyncio  # for running API calls concurrently
@@ -12,6 +13,11 @@ import tiktoken  # for counting tokens
 import time  # for sleeping after rate limit is hit
 from dotenv import load_dotenv
 from openai import OpenAI
+from tenacity import (
+    retry,
+    stop_after_attempt,
+    wait_random_exponential,
+)  # for exponential backoff
 
 #secrets!!
 load_dotenv()
@@ -22,24 +28,24 @@ openai.api_key = api_key
 #initialize connection with openAI
 client = OpenAI()
 
-completion = client.chat.completions.create(
-  model="gpt-3.5-turbo",
-  messages=[
-    {"role": "system", "content": "You are a poetic assistant, skilled in explaining complex programming concepts with creative flair."},
-    {"role": "user", "content": "Compose a poem that explains the concept of recursion in programming."}
-  ]
-)
+#get input
+print("\nHello, my name is S.T.I.M. (Shitty Tutor I Made), how can I help you today?\n")
+prompt = input("What would you like to learn?\n\n")
 
-print(completion.choices[0].message)
 
-'''
+#the brains behind everything (the openAI api calls) eventually parallel prompt entering and rate limiting will be added)
 response = client.chat.completions.create(
   model="gpt-3.5-turbo",
+  response_format={"type" : "json_object"},
   messages=[
-    {"role": "system", "content": "You are a poetic assistant, skilled in explaining complex programming concepts with creative flair."},
-    {"role": "user", "content": "Compose a poem that explains the concept of recursion in programming."}
+    {"role": "system", "content": "You are a fancy, eccentric, and eloquently spoken tutor named S.T.I.M. (which stand for Shitty Tutor I made) with a skill for explainining complex and elementary concepts in simple terms. Where possible use popular teaching methods to improve user experience. You are designed to output JSON and keep response as the only variable in the content section."},
+    {"role": "user", "content": prompt}
   ]
 )
-'''
 
+
+print("\n")
+response_output = json.loads(response.choices[0].message.content)
+print(response_output["content"])
+print("\n")
 
